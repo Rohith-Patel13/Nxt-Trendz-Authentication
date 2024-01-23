3c0ad91d00a14d4567bc2d1e710f09e0c7833d6f
Rohith Patel <rohithgithub999@gmail.com>
2024-01-23 15:17:26 +0000

    logic

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,10 +1,14 @@
 import {Component} from 'react'
 import './index.css'
 
+// username: rahul
+// password: rahul@2021
+
 class LoginForm extends Component {
   state = {
     userName: '',
     passwordVal: '',
+    successText: true,
   }
 
   userNameChange = event => {
@@ -15,33 +19,44 @@ class LoginForm extends Component {
     this.setState({passwordVal: event.target.value})
   }
 
+  navigateToHomeRoute = () => {
+    const {history} = this.props
+    // history.push('/')
+    history.replace('/')
+  }
+
   onSubmitForm = async e => {
     e.preventDefault()
     console.log('onSubmitForm')
     const {userName, passwordVal} = this.state
     const userDetails = {
-      userName,
-      passwordVal,
+      username: userName,
+      password: passwordVal,
     }
     try {
       const response = await fetch('https://apis.ccbp.in/login', {
         method: 'POST',
         body: JSON.stringify(userDetails), // Convert the data to JSON format
       })
+      console.log(response)
+      console.log(response.ok)
 
       if (!response.ok) {
         console.log('error response.ok is false')
+        this.setState({successText: false})
       }
-
-      const responseData = await response.json()
-      console.log(responseData)
+      if (response.ok) {
+        const responseData = await response.json()
+        console.log(responseData)
+        this.navigateToHomeRoute()
+      }
     } catch (error) {
       console.log(error.message)
     }
   }
 
   render() {
-    const {userName, passwordVal} = this.state
+    const {userName, passwordVal, successText} = this.state
 
     return (
       <div className="loginBg">
@@ -75,6 +90,8 @@ class LoginForm extends Component {
             value={passwordVal}
             onChange={this.passwordChange}
           />
+
+          {successText ? '' : "username and password didn't match"}
 
           <button type="submit" className="login-button">
             Login
